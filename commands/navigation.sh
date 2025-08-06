44feb8441136936a50b9b6a870015cf08a91c2a3
Ashutosh Sudhirrao Ardak <ardakashutosh05@gmail.com>
2025-08-06 23:20:24 +0530

    Create navigation.sh

---
+++ commands/navigation.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Navigate to home directory
+cd ~
+
+# Print current directory
+pwd
+
+# List files and directories with details
+ls -l
+
+# Navigate to /tmp and list files
+cd /tmp
+ls -a
